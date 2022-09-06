8a848018b8c1cc794f5c534c56ca0341149f2e8e
mandara11 <mandaram2345@gmail.com>
2022-09-06 21:37:42 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AddressBookSystemADO.NET
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcome to AddressBookSystem ADO.NET");
+        }
+    }
+}
