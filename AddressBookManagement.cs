d69c552577a5f3f10924cd03aac5b7026c86bafd
mandara11 <mandaram2345@gmail.com>
2022-09-06 21:53:54 +0530

    DataBaseConnection

---
+++ AddressBookManagement.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystemADO.NET
+{
+    public class AddressBookManagement
+    {
+        // UC1:- Address Book ServiceDB
+                
+        public static string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=AddressBookDB;Integrated Security=True";
+
+        // Establishing the connection using the Sqlconnection.
+        SqlConnection connection = new SqlConnection(connectionString);
+
+        public void DataBaseConnection()
+        {
+            try
+            {
+                DateTime now = DateTime.Now; //create object DateTime class //DateTime.Now class access system date and time 
+                connection.Open(); // open connection
+                using (connection)  //using SqlConnection
+                {
+                    Console.WriteLine($"Connection is created Successful {now}"); //print msg
+
+                }
+                connection.Close(); //close connection
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
