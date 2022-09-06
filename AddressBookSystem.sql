516ad7f3e8e2afc697516f7b21e28825f93e8c2b
mandara11 <mandaram2345@gmail.com>
2022-09-06 22:07:33 +0530

    Added:UC3

--- AddressBookSystem.sql
+++ AddressBookSystem.sql
@@ -1,5 +1,6 @@
 use AddressBookDB;
 
+--UC1
 ----Create table AddressBookSystem
 Create table AddressBookSystem(
 FirstName varchar(50),
@@ -15,6 +16,23 @@ AddressBookName varchar(50)
 );
 select * from AddressBookSystem;
 
+--UC2
 --insert record into table
 insert into AddressBookSystem values('Mandara','M','smg','Banglore','Kar','577103','9122654458','mandaram@gmail.com','Family','FamilyBook');
 select * from AddressBookSystem;
+
+--UC3
+Create procedure dbo.AddressBookSystemProcedure
+	@FirstName  varchar(150),		
+	@LastName  varchar(150),	
+	@Address  varchar(150),		
+	@City	varchar(150),	
+	@State	varchar(150),	
+	@Zip	varchar(6),
+    @PhoneNumber varchar(50),
+	@EmailId	varchar(150),	
+	@AddressBookType	varchar(150),
+	@AddressBookName	varchar(150)
+as begin
+	Insert into AddressBookSystem values(@FirstName,@LastName,@Address,@City,@State,@Zip,@PhoneNumber,@EmailId,@AddressBookType,@AddressBookName)
+End
--- Program.cs
+++ Program.cs
@@ -12,6 +12,27 @@
 
             //UC2
             addressBookManagement.GetAllContact();
+
+            //UC3
+            AddNewContactDetails();
         }
+        //UC3:Ability to insert new Contacts to Address Book
+        public static void AddNewContactDetails()
+            {
+                AddressBookManagement repository = new AddressBookManagement();
+                AddressBookModel model = new AddressBookModel();
+                model.FirstName = "Ekta";
+                model.LastName = "Kapoor";
+                model.Address = "Durga Kund";
+                model.City = "Varanasi";
+                model.State = "UP";
+                model.Zip = "222105";
+                model.PhoneNumber = "1234567891";
+                model.EmailId = "ekta@gmail.com";
+                model.AddressBookType = "Friend";
+                model.AddressBookName = "Ekta";
+
+                Console.WriteLine(repository.AddDataToTable(model) ? "Record inserted successfully\n" : "Record inserted Failed");
+            }
     }
 }
